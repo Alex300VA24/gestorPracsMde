5542918f392899fb12dcd7ec814b41e4a7ed45f5
Miguel Vega Perez <aries_242015@hotmail.com>
2024-09-19 22:55:36 -0500

    agrega librerias

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Provale</title>
+</head>
+<body>
+    <?php require_once "views/layouts/sideBar.php"?>
+
+    <div class="content">
+        <header class="header">
+            <h1>Sistema de Provale</h1>
+        </header>
+        <main class="main">
+
+        </main>
+    </div>
+</body>
+</html>
